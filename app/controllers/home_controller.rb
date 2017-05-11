cf431a4bed1428f6ca88cd100b2b7e582d9a4bb6
smantri7 <smantri7@gatech.edu>
2017-05-10 20:55:30 -0400

    updated
    updated

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,10 @@
+class HomeController < ApplicationController
+  def index
+  end
+  def chat
+  end
+  def help
+  end
+  def about
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  get 'layouts/application'
+  get 'home/about'
+  get 'home/chat'
+  get 'home/help'
   resources :messages
   resources :users
+  root 'application#application'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
